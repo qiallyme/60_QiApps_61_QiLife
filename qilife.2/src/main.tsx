b9d6aed8d85302bb9d3109264420b8c5c3c0e5ec
Q <crice4485@gmail.com>
2026-07-18 12:56:26 -0400

    feat: enable Cadence UX theme

--- qilife.2/src/main.tsx
+++ qilife.2/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { QiLifeShell } from "./features/qilife/components/QiLifeShell";
 import { AuthProvider } from "./features/qilife/auth/AuthProvider";
 import "./features/qilife/styles/qilife.css";
+import "./features/qilife/styles/cadence.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
